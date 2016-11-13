aa0fe6af7542481702f508ecfada7b78183ef520
jbrownssf <jpbrown@softstackfactory.org>
2016-11-13 06:37:34 +0000

    added puppies

--- src/pages/lobby/lobby.ts
+++ src/pages/lobby/lobby.ts
@@ -1,22 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LandingPage } from '../landing/landing';
+import { PuppyPage } from '../puppy/puppy';
+import { FavoritePage } from '../favorite/favorite'; 
+import { StoreUserInfo } from '../../providers/store-user-info';
 
-/*
-  Generated class for the Lobby page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-lobby',
   templateUrl: 'lobby.html'
 })
 export class LobbyPage {
 
-  constructor(public navCtrl: NavController) {}
-
-  ionViewDidLoad() {
-    console.log('Hello LobbyPage Page');
+  constructor(public navCtrl: NavController,
+              public usersRest:StoreUserInfo
+  
+  
+  ) {
+    this.navCtrl = navCtrl;
+ }
+    puppy(){
+        this.navCtrl.push(PuppyPage);
+    }
+     favorites() {
+        this.navCtrl.push(FavoritePage);
+    }
+    logout() {
+    this.usersRest.logout(window.localStorage.getItem('token'))
+    .map(res => res.json())
+    .subscribe(res => {
+      window.localStorage.clear();
+      this.navCtrl.setRoot(LandingPage);
+    }, err => {
+      console.log("Something went wrong!");
+      window.localStorage.clear();
+      this.navCtrl.setRoot(LandingPage);
+    });
   }
-
 }
